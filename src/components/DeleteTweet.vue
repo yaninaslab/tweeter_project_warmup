99e12007128dc060631b29bdfdd32ac31d9b2a7e
yaninaslab <yana.belash@gmail.com>
2021-12-18 20:33:49 -0500

    Completed DeleteTweet component

--- src/components/DeleteTweet.vue
+++ src/components/DeleteTweet.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-<button @click="delete_tweet">Delete Tweet</button>
-    </div>
+  <div>
+    <button @click="delete_tweet">Delete Tweet</button>
+  </div>
 </template>
 
 <script>
@@ -9,37 +9,37 @@ import axios from "axios";
 import cookies from "vue-cookies";
 axios.defaults.headers.common["X-Api-Key"] =
   "i02PMVITFLBLyry786rGgMUG4xTrG25xAoDXaQ0qURf2d";
-    export default {
-        name: "delete-tweet",
-        methods: {
-            delete_tweet() {
-                axios.request({
-                    url: "https://tweeterest.ga/api/tweets",
-                    method: "DELETE",
-                    data: {
-                        loginToken: this.login_token,
-                        tweetId: this.tweetId,
-                    }
-                })
-                .then(() => {
-        
-          this.$emit('post_deleted', "The post has been successfully deleted!")
-    
+export default {
+  name: "delete-tweet",
+  methods: {
+    delete_tweet() {
+      axios
+        .request({
+          url: "https://tweeterest.ga/api/tweets",
+          method: "DELETE",
+          data: {
+            loginToken: this.login_token,
+            tweetId: `${this.tweetId}`,
+          },
+        })
+        .then(() => {
+          this.$emit("post_deleted", "The post has been successfully deleted!");
         })
         .catch((error) => {
           error.message;
         });
-            }
-        },
-        data() {
-            return {
-                tweetId: undefined,
-                login_token: cookies.get("login_token"),
-            }
-        },
-    }
+    },
+  },
+  props: {
+    tweetId: Number,
+  },
+  data() {
+    return {
+      login_token: cookies.get("login_token"),
+    };
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
--- src/components/UserTweet.vue
+++ src/components/UserTweet.vue
@@ -1,20 +1,21 @@
 <template>
+<div>
+    <h2>{{ success_message }}</h2>
     <div class="tweets_list">
         <article v-for="tweet in tweets" :key="tweet.tweetId">
             <h5>{{ tweet.userId }}</h5>
             <h5>{{ tweet.username }}</h5>
             <p>{{ tweet.content }}</p>
-            <h6>{{ tweet.createdAt}}</h6>
-<delete-tweet @post_deleted="react_to_post_deleted"></delete-tweet>
-<h2>{{ success_message }}</h2>
-        </article>
+            <h6>{{ tweet.createdAt }}</h6>
+<delete-tweet @post_deleted="react_to_post_deleted" :tweetId="tweet.tweetId"></delete-tweet>
 
+        </article>
+    </div>
     </div>
 </template>
 
 <script>
 import axios from "axios";
-import cookies from "vue-cookies";
 axios.defaults.headers.common["X-Api-Key"] =
   "i02PMVITFLBLyry786rGgMUG4xTrG25xAoDXaQ0qURf2d";
 import DeleteTweet from '@/components/DeleteTweet.vue'
@@ -25,13 +26,10 @@ import DeleteTweet from '@/components/DeleteTweet.vue'
         },
         methods: {
             react_to_post_deleted(payload) {
-                if(this.tweetId && cookies.get("login_token"))
                 this.success_message = payload;
             },
         },
-        props: {
-            tweet: Object,
-        },
+
        computed: {
            tweets() {
                return this.$store.state['tweets']; 
